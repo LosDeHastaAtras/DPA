c9e220728131e874d8650a69836dee1d3bdb0e59
Mario Becerra <mariob.c@hotmail.com>
2016-04-03 19:00:48 -0500

    cambia acces por access

--- descarga_estaciones_ecobici.R
+++ descarga_estaciones_ecobici.R
@@ -3,16 +3,16 @@ library(RJSONIO)
 # client_id = "577_4jumf047jam88ksscgkk84sgskwgws0w8ooo4gw84kswk4kokg"
 # client_secret = "1c6nfmvo702s008w0o8gw008k80c8cskogcs0w08s4k0wcokog"
 
-URL_acces_token <- "https://pubsbapi.smartbike.com/oauth/v2/token?client_id=577_4jumf047jam88ksscgkk84sgskwgws0w8ooo4gw84kswk4kokg&client_secret=1c6nfmvo702s008w0o8gw008k80c8cskogcs0w08s4k0wcokog&grant_type=client_credentials"
+URL_access_token <- "https://pubsbapi.smartbike.com/oauth/v2/token?client_id=577_4jumf047jam88ksscgkk84sgskwgws0w8ooo4gw84kswk4kokg&client_secret=1c6nfmvo702s008w0o8gw008k80c8cskogcs0w08s4k0wcokog&grant_type=client_credentials"
 
-acces_token_list <- fromJSON(RCurl::getURL(URL_acces_token))
+access_token_list <- fromJSON(RCurl::getURL(URL_access_token))
 
-acces_token <- acces_token_list$access_token
-refresh_token <- acces_token_list$refresh_token
+access_token <- access_token_list$access_token
+refresh_token <- access_token_list$refresh_token
 
 URL_estaciones <- paste0(
   "https://pubsbapi.smartbike.com/api/v1/stations.json?access_token=",
-  acces_token
+  access_token
 )
 dir <- fromJSON(RCurl::getURL(URL_estaciones))
 
